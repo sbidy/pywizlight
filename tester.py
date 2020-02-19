3278115b00f405ff2a5ca27e9274526b29963cc2
Traub <Stephan.Traub@audius.de>
2020-02-19 10:07:59 +0100

    update code and readme

--- tester.py
+++ tester.py
@@ -2,7 +2,8 @@ import wizconnect
 
 light = wizconnect.wizlight("192.168.178.95")
 #light.setColor(255,255,0,0,50)
-#print(light.getPilot())
-light.lightSwitch()
+#print(light.getState())
+#light.lightSwitch()
 #light.setDimmer(100)
 #light.setColorTemperature(6500)
+#print(light.getBulbConfig())
--- wizconnect.py
+++ wizconnect.py
@@ -1,29 +1,10 @@
 import socket
 import json
 
+
 class wizlight:
     '''
         Creates a instance of a WiZ Light Bulb
-
-        Bulb methodes:
-            getSystemConfig - gets the current system configuration - no paramters need
-            syncPilot - sent by the bulb as heart-beats
-            getPilot - gets the current bulb state - no paramters need to be included
-            setPilot - used to tell the bulb to change color/temp/state
-            Pulse - uncertain of purpose
-            Registration - used to "register" with the bulb: This notifies the built that
-                            it you want it to send you heartbeat sync packets.
-        Bulb paramters:
-            sceneId - calls one of thr predefined scenes (int from 0 to 12?)
-            speed - sets the color changing speed in precent
-            dimming - sets the dimmer of the bulb in precent
-            r - red color range 0-255
-            g - green color range 0-255
-            b - blue color range 0-255
-            c - ????
-            w - white color range 0-255
-            id - the bulb id
-
     '''
     def __init__ (self, ip):
         ''' Constructor with ip of the bulb '''
@@ -58,6 +39,11 @@ class wizlight:
         message = r'{"method":"getPilot","params":{}}'
         return self.sendUDPMessage(message)
 
+    def getBulbConfig(self):
+        ''' returns the configuration from the bulb '''
+        message = r'{"method":"getSystemConfig","params":{}}'
+        return self.sendUDPMessage(message)
+
     def lightSwitch(self):
         '''
         turns the light bulb on or off
@@ -65,9 +51,8 @@ class wizlight:
         # first get the status
         message = r'{"method":"getPilot","params":{}}'
         result = self.sendUDPMessage(message)
-        print(result)
         if result['result']['state']:
-            # it the light is on - switch off
+            # if the light is on - switch off
             message = r'{"method":"setPilot","params":{"state":false}}'
         else:
             # if the light is off - turn on
@@ -75,7 +60,7 @@ class wizlight:
         self.sendUDPMessage(message)
 
     def sendUDPMessage(self, message):
-        ''' send the udp message to the light '''
+        ''' send the udp message to the bulb '''
         UDP_PORT = 38899 # fix port for Wiz Lights
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         sock.sendto(bytes(message, "utf-8"), (self.ip, UDP_PORT))
