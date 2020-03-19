d3df464880a8ebdf3c211514622a6714f04d29f8
Stephan Traub <stetrau@aol.com>
2020-03-19 08:52:43 +0100

    fix for #10 - packet missing in setup.py

--- pywizlight/_version.py
+++ pywizlight/_version.py
@@ -1,3 +1,3 @@
 """PyPi Version"""
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
--- setup.py
+++ setup.py
@@ -27,5 +27,6 @@ setuptools.setup(
      ],
      install_requires=[
           'asyncio',
+          'asyncio-dgram',
       ],
  )
