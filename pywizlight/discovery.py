2fcb924db9758f64401271ec735d1d69724e60bb
Stephan Traub <stetrau@aol.com>
2021-12-17 22:57:32 +0100

    Linting ... again

--- pywizlight/discovery.py
+++ pywizlight/discovery.py
@@ -66,7 +66,7 @@ class BroadcastProtocol(asyncio.DatagramProtocol):
             assert isinstance(
                 transport, BaseTransport
             )  # Required to keep this liskov-safe
-        self.transport = transport # type: ignore
+        self.transport = transport  # type: ignore
         sock = transport.get_extra_info("socket")
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
