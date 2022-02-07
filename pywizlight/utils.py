d92956770c4ef9d01f8adbd6c32b6d441372b725
J. Nick Koston <nick@koston.org>
2022-02-07 02:48:54 -0600

    Add push update support

--- pywizlight/utils.py
+++ pywizlight/utils.py
@@ -1,3 +1,6 @@
+import json
+
+
 def hex_to_percent(hex_value: float) -> float:
     """Convert hex 0-255 values to percent."""
     return round((hex_value / 255) * 100)
@@ -6,3 +9,8 @@ def hex_to_percent(hex_value: float) -> float:
 def percent_to_hex(percent: float) -> int:
     """Convert percent values 0-100 into hex 0-255."""
     return int(round((percent / 100) * 255))
+
+
+def to_wiz_json(dump_obj):
+    """Convert an object to wiz json."""
+    return json.dumps(dump_obj, separators=(",", ":"))
