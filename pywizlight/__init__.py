342c8d46a188ad4165b9703eca52d656101a65b8
4lfach <alfarabibolatbek@gmail.com>
2025-06-17 12:36:33 +0800

    added imports for set_preview_effect

--- pywizlight/__init__.py
+++ pywizlight/__init__.py
@@ -3,6 +3,7 @@ from pywizlight.bulb import PilotBuilder, PilotParser, wizlight
 from pywizlight import discovery
 from pywizlight.scenes import SCENES
 from pywizlight.bulblibrary import BulbType
+from pywizlight.effect_manager import EffectStep, RenderingType, EffectDetails, PreviewEffect
 
 __all__ = [
     "BulbType",
@@ -11,4 +12,8 @@ __all__ = [
     "PilotParser",
     "SCENES",
     "wizlight",
+    "EffectStep",
+    "RenderingType",
+    "EffectDetails",
+    "PreviewEffect",
 ]
